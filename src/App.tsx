1d8f871c8c7c764a924b0ab03c242663b93faa74
maycoll <maycollhidalgo311@gmail.com>
2025-10-13 12:06:36 -0500

    Actualización

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-300">
-      <h1 className="text-4xl font-bold text-blue-900 mb-4">Mi primer proyecto con React + Vite + TypeScript + Tailwind 🚀</h1>
-      <p className="text-lg text-gray-700">Todo listo para comenzar a crear interfaces hermosas y poderosas ✨</p>
+      <h1 className="text-4xl font-bold text-blue-900 mb-4">Mi  Proyecto Final</h1>
+      <p className="text-lg text-gray-700">Proyecto Final</p>
     </div>
   );
 }
